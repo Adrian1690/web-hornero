531dfa72cfa03dddbfbd2c7233d5563f173f7660
Adrian1690 <j.adrian.1690@gmail.com>
2016-09-21 22:37:27 -0500

    our history responsive full

---
+++ cart.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>El Hornero</title>
+
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/mansory.css"/>
+    <link rel="stylesheet" href="css/main.css">
+    
+    
+</head>
+<body cz-shortcut-listen="true">
+	
+	<?php include('header.php'); ?>
+	
+	<!-- <div class="container">
+		
+		<span class="font-GeoSlab703bold"> font-GeoSlab703bold</span><br>
+		<span class="font-GeoSlab703Medium"> font-GeoSlab703Medium</span><br>
+		<span class="font-Montserrat-Bold"> font-Montserrat-Bold</span><br>
+		<span class="font-Montserrat-Hairline"> font-Montserrat-Hairline</span><br>
+		<span class="font-Montserrat-Light"> font-Montserrat-Light</span><br>
+		<span class="font-Montserrat-Regular"> font-Montserrat-Regular</span>
+	</div>
+	-->
+	
+	<section class="backtitle cart parallax">
+		<div class="title">
+			LA CARTA
+			<div class="lineback"></div>
+		</div>
+		
+	</section>
+
+   
+    
+	
+	
+
+	<?php include('footer.php'); ?>
+
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/masonry.pkgd.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/initmansory.js"></script>
+</body>
+</html>
